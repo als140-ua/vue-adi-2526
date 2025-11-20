87e07ffe92d0860c3db546c30551c33eb42a735f
als140-ua <als140@alu.ua.es>
2025-11-20 21:00:42 +0100

    Añadido un selector en el crear noticia para asociar un caballo a una noticia

--- src/views/CrearNoticia.vue
+++ src/views/CrearNoticia.vue
@@ -2,17 +2,39 @@
 
 import NoticiaForm from '../components/NoticiaForm.vue'
 import { useRouter } from 'vue-router';
-import { createNoticia } from '../backend/noticiaService';
+import { createNoticia, createNoticiaCaballo } from '../backend/noticiaService';
 
 const router = useRouter();
 
 async function handleCrearNoticia(payload){
+  const caballoId = payload.caballo_id;
+  delete payload.caballo_id;
+  const params_noticiacaballo = {
+          idNoticia: null, // Se asignará después de crear la noticia
+          idCaballo: caballoId
+  }; 
+  
     try {
-        await createNoticia(payload);
+        const noticia = await createNoticia(payload);
         alert('Noticia creada con éxito');
+
+        params_noticiacaballo.idNoticia = noticia.id;
+        
         router.push('/noticias'); // Redirigir a la página principal de noticias
     } catch (error) {
         alert('Error al crear la noticia: ' + error.message);
+    }
+ 
+    try {
+
+      if (params_noticiacaballo.idNoticia && params_noticiacaballo.idCaballo) {
+          await createNoticiaCaballo(params_noticiacaballo);
+          alert('Noticia asociada al caballo con éxito');
+      }else{
+          console.log('No se pudo asociar la noticia al caballo: faltan IDs');
+      }     
+    } catch (error) {
+        alert('Error al asociar la noticia con el caballo: ' + error.message);
     }
 }
 
